8c7372c607bf0991903d05793d34e19f3a48d062
andrea-artuso <ultrandr3x@gmail.com>
2024-03-10 02:23:09 +0100

    moved local function 'is_valid_date' to functions file

--- controllers/memo/add.controller.php
+++ controllers/memo/add.controller.php
@@ -138,9 +138,4 @@ function format_time($hour, $minute){
 
     return $h.":".$m;
 }
-
-function is_valid_date($date, $format = 'Y-m-d'){
-    $d = DateTime::createFromFormat($format, $date);
-    return $d && $d->format($format) === $date;
-}
 ?>
--- core/functions.php
+++ core/functions.php
@@ -1,25 +1,6 @@
 <?php
 
-function is_valid_date($date){
-    $arr = explode("-", $date);
-
-    if (count($arr) != 3)
-        return false;
-    
-    $gg = intval($arr[2]);
-    $mm = intval($arr[1]);
-    $yy = intval($arr[0]);
-    if ($mm == 4 || $mm == 6 || $mm == 9 || $mm == 11){
-        if ($gg > 30)
-            return false;
-    } else if ($mm == 2){
-        if ($yy % 400 == 0 || ($yy % 4 == 0 && $yy % 100 != 0)){
-            if ($gg > 29)
-                return false;
-        } else if ($gg > 28)
-            return false;
-    } else if ($gg > 31)
-        return false;
-
-    return true;
+function is_valid_date($date, $format = 'Y-m-d'){
+    $d = DateTime::createFromFormat($format, $date);
+    return $d && $d->format($format) === $date;
 }
